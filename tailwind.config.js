7ebd240a2c63143da00d6dcdf23a0f60d8392e9c
Maximilian Scheffler <Maximilian.Scheffler@alho.com>
2023-11-09 12:16:51 +0100

    Projektstart

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+
+      colors: {
+        'theme':'#87ceeb',
+        'theme-alt':'#093ab3',
+      },
+
+    },
+  },
+  plugins: [],
+}
+
