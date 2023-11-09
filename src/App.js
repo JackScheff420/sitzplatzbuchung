e5b55a91efdd9c79ad8100369354e67fe8f9e692
Maximilian Scheffler <Maximilian.Scheffler@alho.com>
2023-11-09 13:15:46 +0100

    theme festgelegt

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import "./style/app.css"
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">
-      Hello world!
-      </h1>
+      <div className="text-secondarytheme bg-primarytheme border-2 border-primarytheme shadow-lg shadow-primarytheme px-4 sm:px-8 py-2 sm:py-3">
+        Hello world!
+      </div>
+      <div className="text-primarytheme bg-accenttheme border-2 border-primarytheme shadow-lg shadow-primarytheme px-4 sm:px-8 py-2 sm:py-3">
+        Hello world!
+      </div>
+      <div className="text-primarytheme bg-secondarytheme border-2 border-primarytheme shadow-lg shadow-primarytheme px-4 sm:px-8 py-2 sm:py-3">
+        Hello world!
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ['"Share Tech"', "sans-serif"],
+    },
+    
     extend: {
 
       colors: {
-        'theme':'#87ceeb',
-        'theme-alt':'#093ab3',
+        'primarytheme':'#fffad6',
+        'secondarytheme':'#374450',
+        'accenttheme' : '#242a22'
       },
 
     },
