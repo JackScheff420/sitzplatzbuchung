fe60e2ec36a99114430a538985b8c2f7e1d46ea3
Maximilian Scheffler <Maximilian.Scheffler@alho.com>
2023-11-20 14:44:47 +0100

    modal angepasst

--- src/App.js
+++ src/App.js
@@ -202,16 +202,16 @@ function App() {
           <div className="fixed inset-0 z-10 overflow-y-auto">
             <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
               <div className="fixed inset-0 transition-opacity">
-                <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+                <div className="absolute inset-0 bg-primarytheme opacity-75"></div>
               </div>
 
-              <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <div className="inline-block align-bottom bg-white border-accenttheme border-2 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                <div className="bg-primarytheme px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                   <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                     <h3 className="text-lg leading-6 font-medium text-gray-900">Der Sitzplatz wurde erfolgreich gebucht</h3>
                   </div>
                 </div>
-                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex items-center justify-center">
+                <div className="bg-primarytheme px-4 py-3 sm:px-6 sm:flex items-center justify-center">
                   <button
                     id='schließen'
                     className='border-accenttheme hover:border-primarytheme border-2 w-36 shadow-lg shadow-accenttheme bg-buttontheme hover:bg-secondarytheme hover:text-buttontheme'
